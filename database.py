15db44cec75ff1b5a4052d096f0037677f168761
sebasfalqueza <sebasfalqueza@gmail.com>
2026-02-11 14:13:11 -0600

    database.py

--- database.py
+++ database.py
@@ -2,38 +2,41 @@ import sqlite3
 import os
 import random
 
-conn = sqlite3.connect('Players.db')
-cursor = conn.cursor()
+def initialize_database():
+    conn = sqlite3.connect('Players.db')
+    cursor = conn.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        name TEXT NOT NULL,
-        numberid INTEGER
-    )
-''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            name TEXT NOT NULL,
+            numberid INTEGER
+        )
+    ''')
 
-for i in range(2): #Loops to add the players in the database with a random ID
-    name = input("Enter name: ") #Adds a player to the database
-    numberid = random.randint(10000000, 99999999) #Generates a random number to be the ID of the player
+    conn.commit()
+    conn.close()
+
+def add_players(name):
+    conn = sqlite3.connect('Players.db')
+    cursor = conn.cursor()
+
+    numberid = random.randint(10000000, 99999999)
 
     cursor.execute('''
         INSERT INTO users (name, numberid)
-        VALUES (?, ?)
+        values (?,?)
     ''', (name, numberid))
 
-conn.commit()
-# conn.close()
+    conn.commit()
+    conn.close()
 
-print("Data added successfully!\n")
+    return numberid
 
-print("Printing data: ")
+def clear_all_players():
+    conn = sqlite3.connect('Players.db')
+    cursor = conn.cursor()
 
-#Displays the Players in the database
-cursor.execute('SELECT * FROM users')
-rows = cursor.fetchall()
+    cursor.execute('DELETE FROM users')
 
-print(f"{'Name:':<20}{'ID:':<10}")
-for row in rows:
-    print(f"{row[0]:<20}{row[1]:<10}")
-
-conn.close()
+    conn.commit()
+    conn.close()
