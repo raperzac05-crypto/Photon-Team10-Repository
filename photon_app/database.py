a30dcb05c0f9a1bc4340ce9eb7ce3ff0310bb20a
sebasfalqueza <sebasfalqueza@gmail.com>
2026-03-02 21:51:18 -0600

    database.py

--- photon_app/database.py
+++ photon_app/database.py
@@ -1,29 +1,26 @@
 import sqlite3
 import os
-import random
 
 def add_players(name):
-    conn = sqlite3.connect('Players.db')
+    conn = sqlite3.connect('photon.db')
     cursor = conn.cursor()
 
-    numberid = random.randint(10000000, 99999999)
-
     cursor.execute('''
-        INSERT INTO users (name, numberid)
-        values (?,?)
-    ''', (name, numberid))
+        INSERT INTO users (name)
+        values (?)
+    ''', (name, ))
 
     conn.commit()
     conn.close()
 
-    return numberid
 
 def clear_all_players():
-    conn = sqlite3.connect('Players.db')
+    conn = sqlite3.connect('photon.db')
     cursor = conn.cursor()
 
     cursor.execute('DELETE FROM users')
 
     conn.commit()
     conn.close()
+
 
