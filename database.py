7ae7bfe04c51a2b03f596cf18276abf3618f559a
sebasfalqueza <sebasfalqueza@gmail.com>
2026-04-02 12:44:19 -0500

    Update database.py

--- database.py
+++ database.py
@@ -21,3 +21,25 @@ def clear_all_players():
 
     conn.commit()
     conn.close()
+
+def get_player(player_id=None, equipment_id=None, codename=None):
+    conn = sqlite3.connect('photon.db')
+    cursor = conn.cursor()
+
+    if player_id:
+        cursor.execute('SELECT * FROM users WHERE playerID = ?', (player_id,))
+        result = cursor.fetchone()
+
+    elif equipment_id:
+        cursor.execute('SELECT * FROM users WHERE equipmentID = ?', (equipment_id,))
+        result = cursor.fetchone()
+
+    elif codename:
+        cursor.execute('SELECT * FROM users WHERE codename = ?', (codename,))
+        result = cursor.fetchone()
+    else:
+        cursor.execute('SELECT * FROM users')
+        result = cursor.fetchall()
+
+    conn.close()
+    return result
